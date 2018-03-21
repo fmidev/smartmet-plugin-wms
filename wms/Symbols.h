13233b3a936a8a066a9956cf94f85deba614541b
Heikki Pernu <pernu@fmi.fi>
2018-03-21 16:57:19 +0000

    Add possibility to define symbols in defs of json

---
+++ wms/Symbols.h
@@ -0,0 +1,37 @@
+// ======================================================================
+/*!
+ * \brief Styles container
+ */
+// ======================================================================
+
+#pragma once
+
+#include <json/json.h>
+
+namespace CTPP
+{
+class CDT;
+}
+
+namespace SmartMet
+{
+namespace Plugin
+{
+namespace Dali
+{
+class Config;
+class State;
+
+class Symbols
+{
+ private:
+  typedef std::map<std::string, std::string> SymbolMap;
+  SymbolMap symbols;
+
+ public:
+  void init(const Json::Value& theJson, const State& theState);
+};
+
+}  // namespace Dali
+}  // namespace Plugin
+}  // namespace SmartMet
